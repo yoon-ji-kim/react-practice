6241fbdc8fe6c8ca730e81310565b97fdc242bd4
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-03-08 15:45:55 +0900

    component #2

--- 2.component/ex01/readme.txt
+++ 2.component/ex01/readme.txt
@@ -7,6 +7,10 @@ ex01. Property (props)
 -   4. 부모가 소유한 데이터
 
 src/01: FoodList: 함수 컨포넌트
+src/02: FoodList: Data Flow(Top=> Down): 함수 컴포넌트
+src/03: FoodList: Data Flow(Top=> Down): 클래스 컴포넌트
+src/04: Property Validation
+    npm i prop-types
 
 =====================================================================================================================================================================================
 $ npm i
@@ -15,7 +19,7 @@ $ npm i
     1) 개발툴
         $ npm i -D webpack webpack-cli webpack-dev-server style-loader css-loader sass-loader node-sass babel-loader @babel/core @babel/cli @babel/preset-env @babel/preset-react
     2) react library
-        $ npm i react react-dom
+        $ npm i react react-dom prop-types
 
 2. webpack.config.js 설정
 3. babel.config.json 설정
--- 2.component/ex01/src/02/FoodList.js
+++ 2.component/ex01/src/02/FoodList.js
@@ -2,11 +2,26 @@ import React from 'react';
 import FoodListItem from './FoodListItem';
 
 function FoodList({foods}) {
+    // const foodItems = [];
+
+    // foods.forEach(function(foods){
+    //     foodItems.push(<FoodListItem name={foods.name} count={foods.count} />)
+    // });
+
+    // const a1 = [1, 2, 3, 4];
+    // const a2= a1.map(function(e){
+    //     return e*e;
+    // })
+
+    // const a3 = foods.map(function(food){
+    //     return <FoodListItem name={foods.name} count={foods.count} />;
+    // })
+
+    //console.log(a3);
+
     return (
          <ul>
-            <FoodListItem name='Bread' count='10' />
-            <FoodListItem name='Egg' count='20' />
-            <FoodListItem name='Milk' count='5' />
+            {foods.map((food) =><FoodListItem name={food.name} count={food.count} />)}
         </ul>
     );
 }
