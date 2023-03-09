037cec161ed2ba03e2e7001939b0727939a6a5c5
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-03-09 14:58:11 +0900

    kanbanboard #2

--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -3,9 +3,10 @@ import styles from './assets/css/KanbanBoard.css';
 import cards from './assets/json/data.json';
 import CardList from './CardList';
 
+
 const KanbanBoard = () => {
     return (
-        <div className={styles.KanbanBoard} >
+        <div className={styles.KanbanBoard}>
             <CardList key={'To Do'} title={'To Do'} cards={cards.filter(card => card.status === 'ToDo')} />
             <CardList key={'Doing'} title={'Doing'} cards={cards.filter(card => card.status === 'Doing')}  />
             <CardList key={'Done'} title={'Done'} cards={cards.filter(card => card.status === 'Done')}  />
