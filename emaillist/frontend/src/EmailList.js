5e11d83627d3d12e7368f3e74a33fc0c108bd464
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-03-09 17:09:01 +0900

    emaillist #3

--- emaillist/frontend/src/EmailList.js
+++ emaillist/frontend/src/EmailList.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './assets/css/EmailList.css';
-import Email from './Email';
 
+//emails.map(email => <Email key={email.no} no={email.no} firstName={email.firstName} lastName={email.lastName} email={email.email} />)
 const EmailList = ({emails}) => {
     console.log(emails);
     return (
         <div>
             <ul className={styles.Emaillist}>
                 {
-                    emails.map(email => <Email key={email.no} no={email.no} firstName={email.firstName} lastName={email.lastName} email={email.email} />)
+                    emails.map(email => <li key={email.no}>
+                        {email.firstName+email.lastName}
+                        <br/>
+                        {email.email}
+                    </li>)
                 }
             </ul>
         </div>
