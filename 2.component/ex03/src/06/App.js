5051946e7d7564a4d30020f53faabfe85b07f61c
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-03-13 13:25:18 +0900

    event #finish

---
+++ 2.component/ex03/src/06/App.js
@@ -0,0 +1,27 @@
+import React, {Component, useRef} from 'react';
+import '../assets/scss/App.scss';
+
+export default class App extends Component {
+    onScroll(e) {
+        console.log(this.outerRef.clientHeight, this.innerRef.clientHeight, this.outerRef.scrollTop);
+    }
+    render() {
+        return (
+            <div
+                className={'App'}
+                ref={ref => this.outerRef = ref}
+                onScroll={this.onScroll.bind(this)}>
+                <div
+                    ref={ref => this.innerRef = ref }>
+                    <ul>
+                        {Array.from({length: 100}, (_, i) => i + 1).map(i =>
+                            <li key={i}>
+                                {`아이템 ${i} 입니다.`}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
