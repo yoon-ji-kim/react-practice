e69d6887de0f24fcf66dff513c34f36ea6589171
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-03-14 16:37:24 +0900

    clock

--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import { func } from 'prop-types';
+import React, {useState, useEffect, useRef} from 'react';
 import './assets/scss/App.scss'
 import Clock from './Clock';
 
@@ -7,19 +8,40 @@ export default function App() {
     const [hours, setHours] = useState(now.getHours());
     const [minutes, setMinutes] = useState(now.getMinutes());
     const [seconds, setSeconds] = useState(now.getSeconds());
-    const [ticks, setTicks] = useState(0);
-    
+    const [ticks, setTicks] = useState(seconds);
+
+    useInterval(() => {
+        setTicks(tick => tick+1);
+        // setTicks(function (tick) {
+        //     return tick + 1;
+        // });
+    }, 1000)
+
+    function useInterval(callback, delay) {
+        //interval 변경하지 않고 최근 interval callback을 가리키는 savedCallback
+        const savedCallback = useRef(); //useRef: 리렌더링 방지
+        useEffect(() => {
+            savedCallback.current = callback;
+        }, [callback]);     //callback 데이터 바뀔때 마다 savad.current 값을 업데이트
+
+        useEffect(() => {
+            function tick() {
+                savedCallback.current();
+            }
+            if(delay !== null) {
+                let id = setInterval(tick, delay);
+                return () => clearInterval(id);
+            }
+        },[delay])
+    }
+
     useEffect(() => {
-        setInterval(function() {
             //세팅할 거 다 하고, ticks 플러스
-                let now = new Date(); 
-                setHours(now.getHours());
-                setMinutes(now.getMinutes());
-                setSeconds(now.getSeconds());
-                setTicks(ticks+1);
-                console.log(ticks);
-            }, 1000);
-    }, [])
+            let now = new Date(); 
+            setHours(now.getHours());
+            setMinutes(now.getMinutes());
+            setSeconds(now.getSeconds());
+    })
     //closer
     return (
         <div>
@@ -27,12 +49,11 @@ export default function App() {
             {
                 ticks % 10 === 0 ?
                 null :
-                <Clock
+                <Clock 
                     message={'ex05: useEffect Hook example'}
                     hours={hours}
                     minutes={minutes}
                     seconds={seconds}/>
-
             }
             
         </div>
