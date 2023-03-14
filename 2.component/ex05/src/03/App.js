02d465b469547ff770031a5967f8f6ea69012991
yoon-ji-kim <yoonjigim0.0@gmail.com>
2023-03-14 14:08:18 +0900

    clock

--- 2.component/ex05/src/03/App.js
+++ 2.component/ex05/src/03/App.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default class App extends Component {
     constructor() {
         super(...arguments);
+        let now = new Date(); 
+        this.state = {
+           hours: now.getHours(),
+           minutes: now.getMinutes(),
+           seconds: now.getSeconds(),
+           session: now.getHours() < 12 ? 'am' : 'pm'
+        }
     }
-
     render() {
         return (
             <div className='clock-display'>
                 <h2>ex05 - Component LifeCycle Practice</h2>
-                    <Clock hours={'11'} minutes={'03'} seconds={'30'} session={'am'} />
+                    <Clock hours={this.state.hours} minutes={this.state.minutes} seconds={this.state.seconds} session={this.state.session} />
             </div>
         );
     }
--- 2.component/ex05/src/03/Clock.js
+++ 2.component/ex05/src/03/Clock.js
@@ -2,25 +2,51 @@ import React, {Component} from 'react';
 import './assets/scss/Clock.scss';
 
 export default class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hours: this.props.hours,
+            minutes : this.props.minutes,
+            seconds : this.props.seconds,
+            session : this.props.seconds
+        }
+    }
+    
+    componentDidMount() {
+        this.timerId = setInterval(() => {
+            let now = new Date(); 
+            this.setState({
+                hours: now.getHours(),
+                minutes : now.getMinutes(),
+                seconds : now.getSeconds(),
+                session : now.getHours() < 12 ? 'am' : 'pm'
+            })
+        }, 1000)
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    
     render() {
         return (
             <div className="clock-field">
                 <div>
-                    <p className="hours">{this.props.hours}</p>
+                    <p className="hours">{this.state.hours}</p>
                     <p className="placeholder"></p>
                 </div>
                 <div className="colon">
                     <p>:</p>
                 </div>
                 <div className="numbers">
-                    <p>{this.props.minutes}</p>
+                    <p>{this.state.minutes}</p>
                     <p className="placeholder"></p>
                 </div>
                 <div className="colon">
                     <p>:</p>
                 </div>
                 <div className="numbers">
-                    <p>{this.props.seconds}</p>
+                    <p>{this.state.seconds}</p>
                     <p className="placeholder"></p>
                 </div>
                 <div className="AmPm">
